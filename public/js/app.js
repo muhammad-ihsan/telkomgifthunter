8d093b87d0d0d7a431b3dd780e7c65290ccc217f
Muhammad Ihsan <muhammad-ihsan@outlook.com>
2017-11-22 11:07:41 +0700

    update success alert

--- public/js/app.js
+++ public/js/app.js
@@ -10,18 +10,12 @@ $(document).ready(function(){
     $('[data-form-registration]').parsley(parsleyOptions)
     .on('field:success', function() {
         console.log(this.$element);
-        // notify('danger', 'Something went wrong please check')
-        // var ok = $('.parsley-error').length === 0;
-        // $('.bs-callout-info').toggleClass('hidden', !ok);
-        // $('.bs-callout-warning').toggleClass('hidden', ok);
     })
     .on('field:error', function() {
         var el = this.$element;
-        // notify('warning', el.name + ' tidak boleh kosong')
-        // console.log('Validation failed for: ', this.$element);
     })
     .on('form:submit', function() {
-        notify('success', 'Wew');
+        notify('success', 'Data Sudah Tersimpan');
         return false;
     });
 });
